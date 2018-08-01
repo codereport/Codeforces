463e004fa6a38a03e2c9605cee9bef9807317dfa
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-08-01 00:48:49 -0400

    Create Round_501_C.cpp

---
+++ Round_501_C.cpp
@@ -0,0 +1,46 @@
+// code_report Solution
+// 
+
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+using vi = vector<int>;
+#define FORI(s,n) for(int i = s; i < n; i++)
+
+void compress_songs (const vi& a, const vi& b, int n, int m) 
+{
+   vi diff (n);
+   FORI (0, n) diff[i] = b[i] - a[i];
+   auto total = accumulate (a.begin (), a.end (), 0LL);
+
+   if (total <= m) {
+      cout << 0 << endl;
+   }
+   else {
+      sort (diff.begin (), diff.end ());
+      int i = 0;
+      while (total > m && i < n) total += diff[i], ++i;
+      cout << (total <= m ? i : -1) << endl;
+   }
+}
+
+int main ()
+{
+   int n, m;
+   long long total = 0;
+   cin >> n >> m;
+   vi A (n), B (n);
+   FORI (0, n) {
+      int a, b; 
+      cin >> a >> b;
+      A[i] = a, B[i] = b;
+   }
+
+   compress_songs (A, B, n, m);
+
+   return 0;
+}

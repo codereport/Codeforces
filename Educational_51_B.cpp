ddd9feec7033cfdd1198656aa865007a4feef657
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-09-22 00:44:37 -0700

    Create Educational_51_B.cpp

---
+++ Educational_51_B.cpp
@@ -0,0 +1,16 @@
+// code_report Solution
+// 
+
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main () {
+   ll l, r;
+   cin >> l >> r;
+   cout << "YES" << endl; 
+   for (ll i = l; i < r + 1; i += 2)
+      cout << i << ' ' << i + 1 << endl;
+   return 0;
+}
